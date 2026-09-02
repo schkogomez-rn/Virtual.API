bf373a14e2cdc3fbb88d7ef65790bde1e0cd2dd1
Schko Gomez <schkogomez@gmail.com>
2026-09-02 10:58:34 -0300

    termino

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const pool = require('./db');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+//Ler dados do banco - Listar produtos - function minhaFuncao()
+app.get('/produtos', (req, res)=>{
+    const sql = 'select * from produtos';
+
+    pool.query(sql, (erro, resultado) => {
+        console.log(resultado);
+        console.log(erro);
+        res.json(resultado.rows);
+    });
+
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando com sucesso em http://localhost:${PORT}`);
+});
