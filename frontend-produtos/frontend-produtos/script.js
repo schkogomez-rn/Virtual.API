684de8f26f545f709de77f117a322eec201fe970
Schko Gomez <schkogomez@gmail.com>
2026-09-08 09:41:49 -0300

    Front-End com async / await e fetch integrado

---
+++ frontend-produtos/frontend-produtos/script.js
@@ -0,0 +1,22 @@
+// A URL da nossa API
+const url = "http://localhost:3000/produtos";
+
+/**
+ * 1. BUSCAR PRODUTOS (GET)
+ */
+async function buscarProdutos() {
+   
+}
+
+
+/**
+ * 2. SALVAR NOVO PRODUTO (POST)
+ */
+const formulario = document.getElementById("formulario");
+
+formulario.addEventListener("submit", async function(evento) {
+
+});
+
+// Executa a busca de produtos assim que a página abre
+buscarProdutos();
